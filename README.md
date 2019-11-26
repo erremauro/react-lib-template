644f86c24d36dfedf8abd8bd6b04f9f0d4ea640b
Roberto Mauro <roberto.mauro@icloud.com>
2019-11-26 14:53:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# React Library Template
+
+A [react]() library template to build react component libraries.
+
+## Installation
+
+To install this template type:
+
+    git clone https://github.com/erremauro/react-lib-template.git <PROJECT_DIR>
+    cd <PROJECT_DIR> && git init && yarn install
+
---
+++ src/components/MyComponent/MyComponent.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './MyComponent.css';
+
+class MyComponent extends Component {
+  static propTypes = {
+    // define your prop types here
+  };
+
+  static defaultProps = {
+    // define your default props values here
+  };
+
+  render() {
+    return(
+      <div className="MyComponent">
+        <h1># MyComponent</h1>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
